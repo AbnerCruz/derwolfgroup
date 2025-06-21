30468e67a60f7aa80ca659daf5c95bcf2e83aa77
Abner <abnercruz4@hotmail.com>
2025-06-21 05:40:19 -0300

    DerwolfBooks Update

--- src/DerwolfBooks/Books.js
+++ src/DerwolfBooks/Books.js
@@ -1,10 +1,16 @@
+export const genre = ["Conto", "Crônica", "Fábula", "Guias/Manuais", "Artigos de Opinião", "Ensaio Reflexivo"];
+export const subGenre = ["Fantasia", "Drama", "Filosofia", "Espiritualidade", "Religião", "Comédia", "Épico"];
 const livros = [
   {
     id: 1,
     nome: "O Solitário Pastor de Ovelhas",
-    descricao: "Ben Afkael narra a história do solitário pastor de ovelhas, cujo cotidiano tranquilo é marcado por memórias, perdas e um encontro misterioso à beira do lago sagrado Sky Mirror. Entre o silêncio das águas e a solidão das colinas, ele revela emoções profundas e a conexão delicada entre passado e presente, amizade e esperança, numa prosa que eterniza a alma de Uriath.",
+    descricao: "Ben Ahfkael narra a história do solitário pastor de ovelhas, cujo cotidiano tranquilo é marcado por memórias, perdas e um encontro misterioso à beira do lago sagrado Sky Mirror. Entre o silêncio das águas e a solidão das colinas, ele revela emoções profundas e a conexão delicada entre passado e presente, amizade e esperança, numa prosa que eterniza a alma de Uriath.",
     arquivo: "/DerwolfBooks/Books/O_Solitario_Pastor_de_Ovelhas.pdf",
     imagem: "/DerwolfBooks/Books/O_Solitario_Pastor_de_Ovelhas.png",
+    filtros: {
+      genre: "Conto",
+      subGenre: ["Fantasia", "Drama"],
+    },
     nota: 9.05,
     disponivel: true,
   },
@@ -14,24 +20,36 @@ const livros = [
     descricao: "",
     arquivo: "/DerwolfBooks/Books/Uriath_Genesis.pdf",
     imagem: "/DerwolfBooks/Books/Uriath_Genesis.png",
+    filtros: {
+      genre: "",
+      subGenre: [],
+    },
     nota: 0,
     disponivel: false,
   },
   {
     id: 3,
     nome: "Estrada Para Danfim",
-    descricao: "Ben Afkael narra a Plendor, seu rei e amigo, um fragmento do sonho que nunca pôde viver: a árdua jornada por uma estrada esquecida em Uriath. Entre pedras e gemidos, exércitos, amantes e fugitivos deixaram suas marcas. É a história de um caminho antigo, de companhias cansadas e destinos incertos, onde a esperança é uma miragem e a memória, o último refúgio dos que não empunham espada.",
+    descricao: "Ben Ahfkael narra a Plendor, seu rei e amigo, um fragmento do sonho que nunca pôde viver: a árdua jornada por uma estrada esquecida em Uriath. Entre pedras e gemidos, exércitos, amantes e fugitivos deixaram suas marcas. É a história de um caminho antigo, de companhias cansadas e destinos incertos, onde a esperança é uma miragem e a memória, o último refúgio dos que não empunham espada.",
     arquivo: "/DerwolfBooks/Books/Estrada_Para_Danfim.pdf",
     imagem: "/DerwolfBooks/Books/Estrada_Para_Danfim.png",
+    filtros: {
+      genre: "Conto",
+      subGenre: ["Fantasia","Drama"],
+    },
     nota: 9.25,
     disponivel: true,
   },
   {
     id: 4,
     nome: "Fundação de Caryndor",
-    descricao: "Ben Afkael narra com reverência e precisão a história de Caryn Ashik, uma mulher que, em meio à guerra e à fuga, ascendeu de refugiada a líder visionária. Contando com a promessa feita ao seu amigo, o Rei Plendor, Ben transporta o leitor para as terras de Caryndor, mostrando a queda do reino e a força das mulheres que o governaram, preservando a memória viva de um tempo marcado por luta, esperança e transformação.",
+    descricao: "Ben Ahfkael narra com reverência e precisão a história de Caryn Ashik, uma mulher que, em meio à guerra e à fuga, ascendeu de refugiada a líder visionária. Contando com a promessa feita ao seu amigo, o Rei Plendor, Ben transporta o leitor para as terras de Caryndor, mostrando a queda do reino e a força das mulheres que o governaram, preservando a memória viva de um tempo marcado por luta, esperança e transformação.",
     arquivo: "/DerwolfBooks/Books/Fundacao_de_Caryndor.pdf",
     imagem: "/DerwolfBooks/Books/Fundacao_de_Caryndor.png",
+    filtros: {
+      genre: "Conto",
+      subGenre: ["Fantasia", "Drama"],
+    },
     nota: 8.6,
     disponivel: true,
   },
@@ -41,6 +59,10 @@ const livros = [
     descricao: "Ben Ahfkael narra a Plendor, seu velho amigo e rei sedento por Uriath, a jornada espiritual e física que o levou a cruzar territórios desconhecidos, enfrentar a guerra e vivenciar o silêncio profundo das batalhas. Relata o peso da memória viva, o encontro com a beleza imprevista após o conflito e a consciência amarga da mortalidade dos companheiros. Seu testemunho é uma oferenda sincera que transcende o tempo e preserva a alma pulsante de Uriath.",
     arquivo: "/DerwolfBooks/Books/O_Lago.pdf",
     imagem: "/DerwolfBooks/Books/O_Lago.png",
+    filtros: {
+      genre: "Conto",
+      subGenre: ["Fantasia","Drama","Filosofia","Guerras"],
+    },
     nota: 10,
     disponivel: true,
   },
@@ -50,15 +72,23 @@ const livros = [
     descricao: "",
     arquivo: "/DerwolfBooks/Books/Thyra.pdf",
     imagem: "/DerwolfBooks/Books/Thyra.png",
+    filtros: {
+      genre: "",
+      subGenre: [],
+    },
     nota: 0,
     disponivel: false,
   },
   {
     id: 7,
     nome: "O Observador",
-    descricao: "Ben Afkael narra, com voz carregada de emoção e respeito, as histórias que testemunhou nas terras de Uriath. Dedicadas a Plendor, seu rei e amigo, suas palavras revelam encontros com figuras enigmáticas como Mikhael, o Observador, cuja missão é testemunhar o mundo sem interferir. Entre sombras e silêncios, Ben registra a essência das vidas ocultas e as duras realidades que o tempo insiste em ocultar.",
+    descricao: "Ben Ahfkael narra, com voz carregada de emoção e respeito, as histórias que testemunhou nas terras de Uriath. Dedicadas a Plendor, seu rei e amigo, suas palavras revelam encontros com figuras enigmáticas como Mikhael, o Observador, cuja missão é testemunhar o mundo sem interferir. Entre sombras e silêncios, Ben registra a essência das vidas ocultas e as duras realidades que o tempo insiste em ocultar.",
     arquivo: "/DerwolfBooks/Books/O_Observador.pdf",
     imagem: "/DerwolfBooks/Books/O_Observador.png",
+    filtros: {
+      genre: "Conto",
+      subGenre: ["Filosofia","Espiritualidade","Drama","Fantasia"],
+    },
     nota: 9.1,
     disponivel: true,
   },
@@ -68,6 +98,10 @@ const livros = [
     descricao: "Ben Ahfkael narra com precisão solene a saga trágica de Melissa e Sabrina, filhas de um reino marcado por pactos sombrios e destinos entrelaçados. Ele revela o preço da salvação, a corrupção da inocência e a luta silenciosa entre luz e sombra, evidenciando o drama de almas condenadas a servir forças antigas. Através de seu relato, o passado esquecidose torna voz viva, e a memória, instrumento de poder e mistério.",
     arquivo: "/DerwolfBooks/Books/O_Cantico_De_Melissa.pdf",
     imagem: "/DerwolfBooks/Books/O_Cantico_De_Melissa.png",
+    filtros: {
+      genre: "Conto",
+      subGenre: ["Drama", "Fantasia"],
+    },
     nota: 8.7,
     disponivel: true,
   },
@@ -77,6 +111,10 @@ const livros = [
     descricao: "Ben Ahfkael narra, dirigindo-se a Plendor, um episódio ocorrido no rigoroso continente de Laekage, onde a jovem Alissa demonstra coragem ímpar ao enfrentar perigos da floresta para salvar o menino Eiran, ameaçado por lobos ferozes. Em meio à natureza implacável e ao inverno severo, ele revela o valor da justiça ativa e da bravura tranquila, capturando a essência da luta humana pela sobrevivência e dignidade num mundo hostil.",
     arquivo: "/DerwolfBooks/Books/O_Grito_da_Floresta.pdf",
     imagem: "/DerwolfBooks/Books/O_Grito_da_Floresta.png",
+    filtros: {
+      genre: "Conto",
+      subGenre: ["Fantasia", "Épico"],
+    },
     nota: 8.3,
     disponivel: true,
   },
@@ -86,6 +124,10 @@ const livros = [
     descricao: "Reflexão contundente sobre a justiça e bondade de Deus na Bíblia, confrontando a ideia de que o Deus do Antigo Testamento seria cruel, em contraste com o Novo. A análise revela que não há contradição moral, mas um intervalo de graça no tempo. Deus não mudou — é justo, santo e misericordioso, e Seus juízos são atos de purificação, não de genocídio. A ira divina permanece viva, aguardando seu tempo escatológico.",
     arquivo: "/DerwolfBooks/Books/A_Justiça_e_a_Bondade_de_Deus_na_Bíblia.pdf",
     imagem: "/DerwolfBooks/Books/A_Justiça_e_a_Bondade_de_Deus_na_Bíblia.png",
+    filtros: {
+      genre: "Ensaio Reflexivo",
+      subGenre: ["Religião", "Espiritual"],
+    },
     nota: 8.9,
     disponivel: true,
   },
@@ -95,9 +137,19 @@ export default livros;
 
 //Faça uma descrição de 500 caracteres no sentido de "Ben Ahfkael narra sobre..."
 
+
+
+
 //
+//De acordo com as opções a seguir quero que você classifique o livro em anexo
+//genre = ["Conto", "Crônica", "Fábula", "Manuais", "Artigos de Opinião", "Ensaio Reflexivo"];
+//subGenre = ["Fantasia", "Drama", "Filosofia", "Espiritualidade", "Guerras", "Religião"]; (Pode ser mais de um subGenre)
 //
-//
+
+
+
+
+
 
 // Você é um avaliador técnico de histórias para a plataforma Derwolf Books. Sua função é atribuir notas de 0 a 10 em 5 critérios abaixo, com base na história que será apresentada a seguir. Depois, calcule a nota final com os pesos indicados.
 
--- src/DerwolfBooks/pages/Home.jsx
+++ src/DerwolfBooks/pages/Home.jsx
@@ -1,25 +1,189 @@
 import { Link } from "react-router-dom";
+import React, { useState } from "react";
 import books from "../Books";
 import "../styles/home.css";
-const sortedBooks = [
-  ...books.filter(b => b.disponivel).sort((a, b) => b.nota - a.nota),
-  ...books.filter(b => !b.disponivel).sort((a, b) => b.nota - a.nota),
-];
+
+import { genre } from "../Books";
+import { subGenre } from "../Books";
+
+
+function shuffleArray(array) {
+  return array
+    .map((item) => ({ item, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(({ item }) => item);
+}
+
 function HomePage() {
+  const [searchText, setSearchText] = useState("");
+  const [selectedgenre, setSelectedgenre] = useState("");
+  const [selectedsubGenre, setSelectedsubGenre] = useState([]);
+  const [sortOrder, setSortOrder] = useState("");
+  const [availabilityFilter, setAvailabilityFilter] = useState("disponivel");
+  const top3Books = [...books]
+  .filter((b) => b.disponivel)
+  .sort((a, b) => b.nota - a.nota)
+  .slice(0, 3);
+
+  const livrosFiltrados = books.filter((livro) => {
+    const { genre, subGenre } = livro.filtros || {};
+
+    const genreOk = selectedgenre === "" || genre === selectedgenre;
+    const subGenreOk =
+      selectedsubGenre.length === 0 ||
+      selectedsubGenre.every((t) => subGenre?.includes(t));
+
+    const availabilityOk =
+      availabilityFilter === "" ||
+      (availabilityFilter === "disponivel" && livro.disponivel) ||
+      (availabilityFilter === "indisponivel" && !livro.disponivel);
+
+    const searchOk =
+      searchText.trim() === "" ||
+      livro.nome.toLowerCase().includes(searchText.toLowerCase()) ||
+      livro.descricao.toLowerCase().includes(searchText.toLowerCase());
+
+      return genreOk && subGenreOk && availabilityOk && searchOk;
+    });
+
+    const isSorting = sortOrder === "asc" || sortOrder === "desc";
+
+    const sortedBooks = isSorting
+      ? [
+          ...livrosFiltrados.filter((b) => b.disponivel).sort((a, b) =>
+            sortOrder === "asc" ? a.nota - b.nota : b.nota - a.nota
+          ),
+          ...livrosFiltrados.filter((b) => !b.disponivel).sort((a, b) =>
+            sortOrder === "asc" ? a.nota - b.nota : b.nota - a.nota
+          ),
+        ]
+      : shuffleArray([
+          ...livrosFiltrados.filter((b) => b.disponivel),
+          ...livrosFiltrados.filter((b) => !b.disponivel),
+    ]);
+  
+  function toggleTema(tema) {
+    setSelectedsubGenre(prev =>
+      prev.includes(tema)
+        ? prev.filter(t => t !== tema)
+        : [...prev, tema]
+    );
+  }
   return (
     <div className="home-container">
       <h1 className="home-title">Derwolf Books</h1>
       <p className="home-subtitle">Escolha um livro para ler ou baixar.</p>
 
+      <div className="top-rated-section">
+        <h2>Melhores</h2>
+        <div className="books-grid">
+          {top3Books.map((livro) => (
+            <div key={livro.id} className="book-card book-card-small">
+              <img src={livro.imagem} alt={`Capa de ${livro.nome}`} className="book-image" />
+              <h2 className="book-title">{livro.nome}</h2>
+              <p className="book-description">{livro.descricao}</p>
+              <p className="book-rating">Nota: {livro.nota} / 10</p>
+
+              <Link to={`/derwolfbooks/books/${livro.id}`} className="book-link">
+                Ler online
+              </Link>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="filters-section">
+        <p>Filtros</p>
+        {/* Grupo: Filtro por gênero */}
+        <div className="filters-group">
+          <h3>Gênero</h3>
+          <select value={selectedgenre} onChange={(e) => setSelectedgenre(e.target.value)}>
+            <option value="" selected>Todos</option>
+            {genre.map(genre => (
+              <option key={genre} value={genre}>{genre}</option>
+            ))}
+          </select>
+        </div>
+
+        {/* Grupo: Filtro por subgêneros */}
+        <div className="filters-group">
+          <h3>Subgêneros</h3>
+          <div className="filters-checkboxes">
+            {subGenre.map((t) => (
+              <label key={t}>
+                <input
+                  type="checkbox"
+                  value={t}
+                  checked={selectedsubGenre.includes(t)}
+                  onChange={() => toggleTema(t)}
+                />
+                {t}
+              </label>
+            ))}
+          </div>
+        </div>
+
+        {/* Grupo: Ordenação */}
+        <div className="filters-group">
+          <h3>Ordenar</h3>
+          <select value={sortOrder} onChange={(e) => setSortOrder(e.target.value)}>
+            <option value="">Aleatório</option>
+            <option value="desc">Maior nota</option>
+            <option value="asc">Menor nota</option>
+          </select>
+        </div>
+
+        <div className="filters-group">
+          <h3>Disponibilidade</h3>
+          <select
+            value={availabilityFilter}
+            onChange={(e) => setAvailabilityFilter(e.target.value)}
+          >
+            <option value="">Todos</option>
+            <option value="disponivel">Disponível</option>
+            <option value="indisponivel">Indisponível</option>
+          </select>
+        </div>
+
+        <div className="filters-group">
+          <button
+            onClick={() => {
+              setSelectedgenre("");
+              setSelectedsubGenre([]);
+              setAvailabilityFilter("disponivel");
+              setSortOrder("");
+            }}
+          >
+            Limpar filtros
+          </button>
+        </div>
+      </div>
+
+      <div className="filters-group" style={{ width: "100%", marginBottom: "1rem" }}>
+        <h3>Pesquisar</h3>
+        <input
+          type="search"
+          placeholder="Digite termo para buscar em título ou descrição..."
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+          style={{
+            width: "100%",
+            padding: "10px",
+            fontSize: "1rem",
+            borderRadius: "8px",
+            border: "1px solid #ccc",
+            boxSizing: "border-box",
+          }}
+          aria-label="Campo de pesquisa por texto"
+          autoComplete="off"
+          spellCheck={false}
+        />
+      </div>
+
       <div className="books-grid">
         {sortedBooks.map((livro) => (
-          <div key={livro.id} className="book-card">
-            <img
-              src={livro.imagem}
-              alt={`Capa de ${livro.nome}`}
-              className="book-image"
-              style={{ borderRadius: "20px" }}
-            />
+          <div key={livro.id} className="book-card book-card-large">
+            <img src={livro.imagem} alt={`Capa de ${livro.nome}`} className="book-image" />
             <h2 className="book-title">{livro.nome}</h2>
             <p className="book-description">{livro.descricao}</p>
             <p className="book-rating">Nota: {livro.nota} / 10</p>
@@ -29,11 +193,7 @@ function HomePage() {
                 Ler online
               </Link>
             ) : (
-              <button
-                disabled
-                className="book-link book-link-disabled"
-                title="Livro indisponível no momento"
-              >
+              <button disabled className="book-link book-link-disabled">
                 Indisponível
               </button>
             )}
